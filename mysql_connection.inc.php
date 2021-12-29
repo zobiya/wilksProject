7cbe3169b6e69abcbdaf50636d48a79798f4657d
Billy Yang <31418367+zobiya@users.noreply.github.com>
2021-12-28 20:08:29 -0600

    Update mysql_connection.inc.php

--- mysql_connection.inc.php
+++ mysql_connection.inc.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_NAME', 'project_database');
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'Bronzetree789');
+define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
 
 $db = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
